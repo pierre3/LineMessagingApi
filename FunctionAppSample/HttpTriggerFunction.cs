2a881722426d998260edd65abae8b768bb3ad548
pierre3 <pierre3@outlook.com>
2017-08-21 23:01:56 +0900

    !^^

--- FunctionAppSample/HttpTriggerFunction.cs
+++ FunctionAppSample/HttpTriggerFunction.cs
@@ -8,46 +8,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Security.Cryptography;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace FunctionAppSample
 {
-    internal static class LineMessagingService
-    {
-
-        public static void Initialize(string channelAccessToken)
-        {
-        }
-
-        public static async Task<bool> VerifySignature(HttpRequestMessage req, string channelSecret)
-        {
-            var content = await req.Content.ReadAsStringAsync();
-            var xLineSignature = req.Headers.GetValues("X-Line-Signature").FirstOrDefault();
-            return VerifySignature(channelSecret, xLineSignature, content);
-        }
-
-        private static bool VerifySignature(string channelSecret, string xLineSignature, string requestBody)
-        {
-            try
-            {
-                var key = Encoding.UTF8.GetBytes(channelSecret);
-                var body = Encoding.UTF8.GetBytes(requestBody);
-
-                using (HMACSHA256 hmac = new HMACSHA256(key))
-                {
-                    var hash = hmac.ComputeHash(body, 0, body.Length);
-                    var hash64 = Convert.ToBase64String(hash);
-                    return xLineSignature == hash64;
-                }
-            }
-            catch
-            {
-                return false;
-            }
-        }
-    }
     public static class HttpTriggerFunction
     {
         static LineMessagingClient lineMessagingClient;
