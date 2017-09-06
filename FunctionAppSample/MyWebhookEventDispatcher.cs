7f2d587c1796a33bced90542e6ede4701014f67f
pierre3 <pierre3@outlook.com>
2017-09-06 22:23:34 +0900

    Simplify a WebhookEventSource class

--- FunctionAppSample/MyWebhookEventDispatcher.cs
+++ FunctionAppSample/MyWebhookEventDispatcher.cs
@@ -21,21 +21,33 @@ namespace FunctionAppSample
             Log = log;
         }
 
+        private Task EchoAsync(string replyToken, string userMessage)
+        {
+            return MessagingClient.ReplyMessageAsync(replyToken, userMessage);
+        }
+
         protected override async Task OnMessageAsync(MessageEvent ev)
         {
-            Log.WriteInfo($"SourceType:{ev.Source.Type}, UserId:{ev.Source.UserId}, MessageType:{ev.Message.Type}");
+            Log.WriteInfo($"SourceType:{ev.Source.Type}, EntryId:{ev.Source.EntryId}, MessageType:{ev.Message.Type}");
 
             switch (ev.Message.Type)
             {
                 case EventMessageType.Text:
                     var textMessage = (TextEventMessage)ev.Message;
-                    await MessagingClient.ReplyMessageAsync(ev.ReplyToken, textMessage.Text);
+
+
+                    await EchoAsync(ev.ReplyToken, textMessage.Text);
+
                     break;
                 case EventMessageType.Image:
                 case EventMessageType.Audio:
                 case EventMessageType.Video:
                 case EventMessageType.File:
+                    break;
                 case EventMessageType.Location:
+                    var locMessage = (LocationEventMessage)ev.Message;
+                    await MessagingClient.ReplyMessageAsync(ev.ReplyToken, $"https://www.google.co.jp/maps/@{locMessage.Latitude}.{locMessage.Longitude}");
+                    break;
                 case EventMessageType.Sticker:
                     break;
             }
@@ -43,14 +55,14 @@ namespace FunctionAppSample
 
         protected override async Task OnFollowAsync(FollowEvent ev)
         {
-            Log.WriteInfo($"SourceType:{ev.Source.Type}, UserId:{ev.Source.UserId}");
+            Log.WriteInfo($"SourceType:{ev.Source.Type}, EntryId:{ev.Source.EntryId}");
 
-            await TableStorage.AddEntryAsync(ev.Source.Type.ToString(), ev.Source.UserId);
+            await TableStorage.AddEntryAsync(ev.Source.Type.ToString(), ev.Source.EntryId);
 
             var userName = "";
-            if (!string.IsNullOrEmpty(ev.Source.UserId))
+            if (!string.IsNullOrEmpty(ev.Source.EntryId))
             {
-                var userProfile = await MessagingClient.GetUserProfileAsync(ev.Source.UserId);
+                var userProfile = await MessagingClient.GetUserProfileAsync(ev.Source.EntryId);
                 userName = userProfile?.DisplayName ?? "";
             }
 
@@ -59,36 +71,22 @@ namespace FunctionAppSample
 
         protected override async Task OnUnfollowAsync(UnfollowEvent ev)
         {
-            Log.WriteInfo($"SourceType:{ev.Source.Type}, UserId:{ev.Source.UserId}");
-
-            await TableStorage.DeleteEntryAsync(ev.Source.Type.ToString(), ev.Source.UserId);
-        }
-
-        protected override async Task OnLeaveAsync(LeaveEvent ev)
-        {
-            var entryId = (ev.Source.Type == EventSourceType.Group) ?
-                ((SourceGroup)ev.Source).GroupId :
-                ((SourceRoom)ev.Source).RoomId;
-            Log.WriteInfo($"SourceType:{ev.Source.Type}, UserId:{entryId}");
-
-            await TableStorage.DeleteEntryAsync(ev.Source.Type.ToString(), entryId);
+            Log.WriteInfo($"SourceType:{ev.Source.Type}, EntryId:{ev.Source.EntryId}");
+            await TableStorage.DeleteEntryAsync(ev.Source.Type.ToString(), ev.Source.EntryId);
         }
 
         protected override async Task OnJoinAsync(JoinEvent ev)
         {
-            if (ev.Source.Type == EventSourceType.Group)
-            {
-                var groupId = ((SourceGroup)ev.Source).GroupId;
-                Log.WriteInfo($"SourceType:{ev.Source.Type}, UserId:{groupId}");
-                await MessagingClient.ReplyMessageAsync(ev.ReplyToken, $"Thank you for letting me join your group!");
-            }
-            else if (ev.Source.Type == EventSourceType.Room)
-            {
-                var roomId = ((SourceRoom)ev.Source).RoomId;
-                Log.WriteInfo($"SourceType:{ev.Source.Type}, UserId:{roomId}");
-                await MessagingClient.ReplyMessageAsync(ev.ReplyToken, $"Thank you for letting me join your room!");
-            }
+            Log.WriteInfo($"SourceType:{ev.Source.Type}, EntryId:{ev.Source.EntryId}");
+            await MessagingClient.ReplyMessageAsync(ev.ReplyToken, $"Thank you for letting me join your {ev.Source.Type.ToString().ToLower()}!");
         }
+
+        protected override async Task OnLeaveAsync(LeaveEvent ev)
+        {
+            Log.WriteInfo($"SourceType:{ev.Source.Type}, EntryId:{ev.Source.EntryId}");
+            await TableStorage.DeleteEntryAsync(ev.Source.Type.ToString(), ev.Source.EntryId);
+        }
+
         protected override Task OnBeaconAsync(BeaconEvent ev)
         {
             throw new NotImplementedException();
