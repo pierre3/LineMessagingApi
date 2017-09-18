267241b1db1765fed4ae0a529c21d21d64288b30
pierre3 <pierre3@outlook.com>
2017-09-18 22:00:50 +0900

    Add sample code  (reply random sticker, reply  imagemap)

--- FunctionAppSample/LineBotApp.cs
+++ FunctionAppSample/LineBotApp.cs
@@ -3,6 +3,9 @@ using Line.Messaging.Webhooks;
 using Microsoft.Azure.WebJobs.Host;
 using Newtonsoft.Json;
 using System;
+using System.Drawing;
+using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FunctionAppSample
@@ -41,7 +44,8 @@ namespace FunctionAppSample
                     break;
 
                 case EventMessageType.Image:
-                    await EchoImageAsync(ev.ReplyToken, ev.Message.Id, blobDirectoryName);
+                    //await EchoImageAsync(ev.ReplyToken, ev.Message.Id, blobDirectoryName);
+                    await ReplyImagemapAsync(ev.ReplyToken, ev.Message.Id, blobDirectoryName);
                     break;
 
                 case EventMessageType.Audio:
@@ -55,9 +59,26 @@ namespace FunctionAppSample
                     break;
 
                 case EventMessageType.Sticker:
+                    await ReplyRandomSticker(ev.ReplyToken);
                     break;
 
             }
+        }
+
+        private async Task ReplyRandomSticker(string replyToken)
+        {
+            //Sticker ID of bssic stickers (packge ID =1)
+            //see https://devdocs.line.me/files/sticker_list.pdf
+            var stickerids = Enumerable.Range(1, 17)
+                .Concat(Enumerable.Range(21, 1))
+                .Concat(Enumerable.Range(100, 139 - 100 + 1))
+                .Concat(Enumerable.Range(401, 430 - 400 + 1)).ToArray();
+
+            var rand = new Random(Guid.NewGuid().GetHashCode());
+            var stickerId = stickerids[rand.Next(stickerids.Length - 1)].ToString();
+            await MessagingClient.ReplyMessageAsync(replyToken, new[] {
+                        new StickerMessage("1", stickerId)
+                    });
         }
 
         protected override async Task OnFollowAsync(FollowEvent ev)
@@ -105,13 +126,14 @@ namespace FunctionAppSample
             Log.WriteInfo($"SourceType:{ev.Source.Type}, EntryId:{ev.Source.Id}");
 
             var data = JsonConvert.DeserializeAnonymousType(ev.Postback.Data, new { type = "", searchWord = "", location = "" });
+            var searchword = Uri.EscapeUriString(Regex.Replace(data.searchWord, "\\s", "+"));
             if (data.type == "keyword")
             {
-                await MessagingClient.ReplyMessageAsync(ev.ReplyToken, $"https://www.google.co.jp/maps/search/{data.searchWord}/@{data.location}");
+                await MessagingClient.ReplyMessageAsync(ev.ReplyToken, $"https://www.google.co.jp/maps/search/?api=1&query={data.searchWord}&query={data.location}");
             }
             else
             {
-                await MessagingClient.ReplyMessageAsync(ev.ReplyToken, $"https://www.google.co.jp/maps/dir/{data.location}/{data.searchWord}");
+                await MessagingClient.ReplyMessageAsync(ev.ReplyToken, $"https://www.google.co.jp/maps/dir/?api=1&origin={data.location}&destination={searchword}");
             }
 
             await TableStorage.UpdateEntryAsync(new LineBotEntry(ev.Source.Type.ToString(), ev.Source.Id) { Location = null });
@@ -138,20 +160,18 @@ namespace FunctionAppSample
                 new ConfirmTemplate($"Select a search type.",
                     new[]
                     {
-                                    new PostbackTemplateAction("Keyword",JsonConvert.SerializeObject(
-                                        new
-                                        {
-                                            type = "keyword",
-                                            searchWord,
-                                            location = entry.Location
-                                        }),null),
-                                    new PostbackTemplateAction("Route",JsonConvert.SerializeObject(
-                                        new
-                                        {
-                                            type = "route",
-                                            searchWord,
-                                            location = entry.Location
-                                        }),null)
+                        new PostbackTemplateAction("Keyword",JsonConvert.SerializeObject(
+                            new {
+                                type = "keyword",
+                                searchWord,
+                                location = entry.Location
+                            }),null),
+                        new PostbackTemplateAction("Route",JsonConvert.SerializeObject(
+                            new {
+                                type = "route",
+                                searchWord,
+                                location = entry.Location
+                            }),null)
                     }));
             await MessagingClient.ReplyMessageAsync(replyToken, new[] { templateMessage });
         }
@@ -163,13 +183,45 @@ namespace FunctionAppSample
 
             var imageStream = await MessagingClient.GetContentStreamAsync(messageId);
 
-            var image = System.Drawing.Image.FromStream(imageStream);
+            var image = Image.FromStream(imageStream);
             var previewImage = image.GetThumbnailImage((int)(image.Width * 0.25), (int)(image.Height * 0.25), () => false, IntPtr.Zero);
 
             var blobImagePath = await BlobStorage.UploadImageAsync(image, blobDirectoryName, imageName);
             var blobPreviewPath = await BlobStorage.UploadImageAsync(previewImage, blobDirectoryName, previewImageName);
 
             await MessagingClient.ReplyMessageAsync(replyToken, new[] { new ImageMessage(blobImagePath.ToString(), blobPreviewPath.ToString()) });
+        }
+
+        private async Task ReplyImagemapAsync(string replyToken, string messageId, string blobDirectoryName)
+        {
+            var imageStream = await MessagingClient.GetContentStreamAsync(messageId);
+            var image = Image.FromStream(imageStream);
+
+            var uri = await UploadImageAsync(1040);
+            await UploadImageAsync(700);
+            await UploadImageAsync(460);
+            await UploadImageAsync(300);
+            await UploadImageAsync(240);
+            var imageSize = new ImagemapSize(1024, (int)(1040 * (double)image.Height / image.Width));
+            var areaWidth = imageSize.Width / 2;
+            var areaHeight = imageSize.Height / 2;
+            var imagemapMessage = new ImagemapMessage(uri.ToString().Replace("/1040", ""),
+                "Sample Imagemap",
+                imageSize,
+                new IImagemapAction[] {
+                    new MessageImagemapAction(new ImagemapArea(0, 0, areaWidth,areaHeight),"Area Top-Left"),
+                    new MessageImagemapAction(new ImagemapArea(areaWidth, 0, areaWidth,areaHeight),"Area Top-Right"),
+                    new MessageImagemapAction(new ImagemapArea(0, areaHeight, areaWidth,areaHeight),"Area Bottom-Left"),
+                    new MessageImagemapAction(new ImagemapArea(areaWidth, areaHeight, areaWidth,areaHeight),"Area Bottom-Right"),
+                });
+
+            await MessagingClient.ReplyMessageAsync(replyToken, new[] { imagemapMessage });
+
+            async Task<Uri> UploadImageAsync(int baseSize)
+            {
+                var img = image.GetThumbnailImage(baseSize, image.Height * 1040 / image.Width, () => false, IntPtr.Zero);
+                return await BlobStorage.UploadImageAsync(img, blobDirectoryName + "/" + messageId, baseSize.ToString());
+            }
         }
 
         private async Task UploadMediaContentAsync(string replyToken, string messageId, string blobDirectoryName, string blobName)
