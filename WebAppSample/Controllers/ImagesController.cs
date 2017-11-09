38e71ec1fa0986ed26091a4d04b3f88801c432b2
Kenichiro Nakamura <kenakamu@microsoft.com>
2017-11-09 10:56:10 +0900

    Add WebAppSample

---
+++ WebAppSample/Controllers/ImagesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace WebAppSample.Controllers
+{
+    public class ImagesController : ApiController
+    {
+        [HttpGet]
+        public async Task<HttpResponseMessage> Get(string file, string size)
+        {
+            var root = AppDomain.CurrentDomain.SetupInformation.ApplicationBase;
+            var path = Path.Combine(root, "Images", file, size + ".png");
+
+            var response = Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StreamContent(new FileStream(path, FileMode.Open, FileAccess.Read));
+            response.Content.Headers.ContentDisposition = new System.Net.Http.Headers.ContentDispositionHeaderValue("attachment");
+            response.Content.Headers.ContentDisposition.FileName = $"{size}.png";
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("image/png");
+
+            return response;
+        }
+    }
+}
