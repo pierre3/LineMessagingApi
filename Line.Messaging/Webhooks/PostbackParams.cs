c874e82518ca4d6923737024c4a6f7d6d21a7901
pierre3 <pierre3@outlook.com>
2018-01-10 23:33:47 +0900

    Add date time validation at constructor of PostbackParams

--- Line.Messaging/Webhooks/PostbackParams.cs
+++ Line.Messaging/Webhooks/PostbackParams.cs
@@ -1,4 +1,7 @@
-﻿namespace Line.Messaging.Webhooks
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Line.Messaging.Webhooks
 {
     /// <summary>
     /// Object with the date and time selected by a user through a datetime picker action. The full-date, time-hour, and time-minute formats follow the RFC3339 protocol.
@@ -22,6 +25,19 @@
 
         public PostbackParams(string date, string time, string datetime)
         {
+            if (date != null && !Regex.Match(date, @"^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])$").Success)
+            {
+                throw new ArgumentException($"Date format must be \"yyyy-MM-dd\".", nameof(date));
+            }
+            if (time != null && !Regex.Match(time, @"^([01][0-9]|2[0-3]):([0-5][0-9])$").Success)
+            {
+                throw new ArgumentException($"Time format must be \"HH:mm\".", nameof(time));
+            }
+            if (datetime != null && !Regex.Match(datetime, @"^(\d{4})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])T([01][0-9]|2[0-3]):([0-5][0-9])$").Success)
+            {
+                throw new ArgumentException("Date-Time format must be \"yyyy-MM-ddTHH:mm\".", nameof(datetime));
+            }
+
             Date = date;
             Time = time;
             DateTime = datetime;
