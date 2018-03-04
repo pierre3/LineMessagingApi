01ab94ec156504c662f47b8f35919b378ed725c2
pierre3 <pierre3@outlook.com>
2018-03-04 22:55:02 +0900

    Update project templates for SDK v1.2.4

--- FunctionAppSample/HttpTriggerFunction.cs
+++ FunctionAppSample/HttpTriggerFunction.cs
@@ -4,8 +4,6 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.Azure.WebJobs.Host;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -30,14 +28,22 @@ namespace FunctionAppSample
             {
                 var channelSecret = System.Configuration.ConfigurationManager.AppSettings["ChannelSecret"];
                 var events = await req.GetWebhookEventsAsync(channelSecret);
+                var connectionString = System.Configuration.ConfigurationManager.AppSettings["AzureWebJobsStorage"];
+                var botStatus = await TableStorage<BotStatus>.CreateAsync(connectionString, "botstatus");
+                var blobStorage = await BlobStorage.CreateAsync(connectionString, "linebotcontainer");
 
-                var app = await SwitchAppsAsync(events, log);
+                var app = new LineBotApp(lineMessagingClient, botStatus, blobStorage, log);
+
+                /* To run sample apps in the samples directory, comment out the above line and cancel this comment out.
+                var app = await AppSwitcher.SwitchAppsAsync(events,lineMessagingClient, botStatus, blobStorage, log);
+                //*/
+
                 await app.RunAsync(events);
 
             }
             catch (InvalidSignatureException e)
             {
-                return req.CreateResponse(HttpStatusCode.Forbidden, new { Message = e.Message });
+                return req.CreateResponse(HttpStatusCode.Forbidden, new { e.Message });
             }
             catch (LineResponseException e)
             {
@@ -45,7 +51,7 @@ namespace FunctionAppSample
                 var debugUserId = System.Configuration.ConfigurationManager.AppSettings["DebugUser"];
                 if (debugUserId != null)
                 {
-                    await lineMessagingClient.PushMessageAsync(debugUserId, @"{e.StatusCode}({(int)e.StatusCode}), {e.ResponseMessage.ToString()}");
+                    await lineMessagingClient.PushMessageAsync(debugUserId, $"{e.StatusCode}({(int)e.StatusCode}), {e.ResponseMessage.ToString()}");
                 }
             }
             catch (Exception e)
@@ -61,73 +67,7 @@ namespace FunctionAppSample
             return req.CreateResponse(HttpStatusCode.OK);
         }
 
-        private static async Task<WebhookApplication> SwitchAppsAsync(IEnumerable<WebhookEvent> events, TraceWriter log)
-        {
-            var connectionString = System.Configuration.ConfigurationManager.AppSettings["AzureWebJobsStorage"];
-            var botStatus = await TableStorage<BotStatus>.CreateAsync(connectionString, "botstatus");
-            var blobStorage = await BlobStorage.CreateAsync(connectionString, "linebotcontainer");
-
-            var ev = events.First();
-            var status = await botStatus.FindAsync(ev.Source.Type.ToString(), ev.Source.Id);
-            if (status == null)
-            {
-                status = new BotStatus()
-                {
-                    SourceType = ev.Source.Type.ToString(),
-                    SourceId = ev.Source.Id,
-                    CurrentApp = "@"
-                };
-            }
-
-            var message = (ev as MessageEvent)?.Message as TextEventMessage;
-            var text = message?.Text;
-            if (text == null || !text.StartsWith("@"))
-            {
-                text = status.CurrentApp ?? "@";
-            }
-            text = text.Trim().ToLower();
-            WebhookApplication app = null;
-
-            if (text != "@richmenu")
-            {
-                await lineMessagingClient.UnLinkRichMenuFromUserAsync(ev.Source.Id);
-            }
-
-            switch (text)
-            {
-                case "@":
-                    app = new LineBotApp(lineMessagingClient, botStatus, blobStorage, log);
-                    break;
-                case "@buttons":
-                    app = new ButtonsTemplateSampleApp(lineMessagingClient, blobStorage, log);
-                    break;
-                case "@carousel":
-                    app = new CarouselTemplateSampleApp(lineMessagingClient, blobStorage, log);
-                    break;
-                case "@postback":
-                    app = new PostbackMessageSampleApp(lineMessagingClient, botStatus, log);
-                    break;
-                case "@imagemap":
-                    app = new ImagemapSampleApp(lineMessagingClient, blobStorage, log);
-                    break;
-                case "@imagecarousel":
-                    app = new ImageCarouselSampleApp(lineMessagingClient, blobStorage, log);
-                    break;
-                case "@datetime":
-                    app = new DateTimePickerSampleApp(lineMessagingClient, log);
-                    break;
-                case "@richmenu":
-                    app = new RichMenuSampleApp(lineMessagingClient, log);
-                    break;
-                default:
-                    text = "@";
-                    app = new LineBotApp(lineMessagingClient, botStatus, blobStorage, log);
-                    break;
-            }
-            status.CurrentApp = text;
-            await botStatus.UpdateAsync(status);
-            return app;
-        }
+        
     }
 
 }
---
+++ FunctionAppSample/Samples/AppSwitcher.cs
@@ -0,0 +1,88 @@
+﻿using Line.Messaging;
+using Line.Messaging.Webhooks;
+using Microsoft.Azure.WebJobs.Host;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace FunctionAppSample
+{
+    public static class AppSwitcher
+    {
+        public static async Task<WebhookApplication> SwitchAppsAsync(IEnumerable<WebhookEvent> events, LineMessagingClient line, TableStorage<BotStatus> botStatus, BlobStorage blobStorage, TraceWriter log)
+        {
+
+            var ev = events.First();
+            var status = await botStatus.FindAsync(ev.Source.Type.ToString(), ev.Source.Id);
+            if (status == null)
+            {
+                status = new BotStatus()
+                {
+                    SourceType = ev.Source.Type.ToString(),
+                    SourceId = ev.Source.Id,
+                    CurrentApp = "@"
+                };
+            }
+
+            var message = (ev as MessageEvent)?.Message as TextEventMessage;
+            var text = message?.Text;
+            if (text == null || !text.StartsWith("@"))
+            {
+                text = status.CurrentApp;
+            }
+            text = text.Trim().ToLower();
+            WebhookApplication app = null;
+
+            if (text != "@richmenu")
+            {
+                try
+                {
+                    await line.UnLinkRichMenuFromUserAsync(ev.Source.Id);
+                }
+                catch (LineResponseException e)
+                {
+                    if (e.StatusCode != HttpStatusCode.NotFound)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            switch (text)
+            {
+                case "@":
+                    app = new LineBotApp(line, botStatus, blobStorage, log);
+                    break;
+                case "@buttons":
+                    app = new ButtonsTemplateSampleApp(line, blobStorage, log);
+                    break;
+                case "@carousel":
+                    app = new CarouselTemplateSampleApp(line, blobStorage, log);
+                    break;
+                case "@postback":
+                    app = new PostbackMessageSampleApp(line, botStatus, log);
+                    break;
+                case "@imagemap":
+                    app = new ImagemapSampleApp(line, blobStorage, log);
+                    break;
+                case "@imagecarousel":
+                    app = new ImageCarouselSampleApp(line, blobStorage, log);
+                    break;
+                case "@datetime":
+                    app = new DateTimePickerSampleApp(line, log);
+                    break;
+                case "@richmenu":
+                    app = new RichMenuSampleApp(line, log);
+                    break;
+                default:
+                    text = "@";
+                    app = new LineBotApp(line, botStatus, blobStorage, log);
+                    break;
+            }
+            status.CurrentApp = text;
+            await botStatus.UpdateAsync(status);
+            return app;
+        }
+    }
+}
