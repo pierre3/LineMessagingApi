b0b5cfe5897e67ec72520743657ca388ee328d92
pierre3 <pierre3@outlook.com>
2018-04-07 17:49:18 +0900

    Add account link APIs

---
+++ Line.Messaging/Webhooks/AccountLinkEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace Line.Messaging.Webhooks
+{
+    /// <summary>
+    /// Event object for when a user has linked his/her LINE account with a provider's service account. You can reply to account link events.
+    /// If the link token has expired or has already been used, no webhook event will be sent and the user will be shown an error.
+    /// </summary>
+    public class AccountLinkEvent : ReplyableEvent
+    {
+        /// <summary>
+        /// Link Object
+        /// </summary>
+        public Link Link { get; }
+
+        public AccountLinkEvent(WebhookEventSource source, long timestamp, string replyToken, Link link)
+            : base(WebhookEventType.AccountLink, source, timestamp, replyToken)
+        {
+            Link = link;
+        }
+    }
+}
