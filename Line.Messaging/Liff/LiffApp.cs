1bef79427bccd714e6680c7ee248e60065a53e4c
pierre3 <pierre3@outlook.com>
2018-06-08 22:12:10 +0900

    Add LIFF Server API

---
+++ Line.Messaging/Liff/LiffApp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Line.Messaging.Liff
+{
+    public class LiffApp
+    {
+        public string LiffId { get; }
+        public View View { get; }
+
+        public LiffApp(string liffId, View view)
+        {
+            LiffId = liffId;
+            View = view;
+        }
+    }
+}
