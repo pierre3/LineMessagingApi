f2db2659cd23b8bab85006e8ab24dbd0b451a04c
pierre3 <pierre3@outlook.com>
2018-06-19 22:43:34 +0900

    Add LIFF Manager (WPF Application)

---
+++ LiffManager/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LiffManager
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MainWindowViewModel VM { get => DataContext as MainWindowViewModel; }
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ LiffManager/MainWindowViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+using Line.Messaging.Liff;
+using System.Windows.Input;
+using Prism.Commands;
+using Line.Messaging;
+
+namespace LiffManager
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private LiffClient _liffClient;
+        private LineMessagingClient _lineClient;
+        private string _liffClientAccessToken;
+        private string _lineClientAccessToken;
+
+        private ViewType _selectedViewType = ViewType.Compact;
+        private string _siteUrl = "";
+        private string _channelAccessToken = "";
+        private string _userId;
+
+        private IList<LiffApp> _liffApps;
+        private LiffApp _selectedLiffApp;
+
+        private LiffClient LiffClient
+        {
+            get
+            {
+                if (_liffClient == null || _liffClientAccessToken != _channelAccessToken)
+                {
+                    _liffClient = new LiffClient(_channelAccessToken);
+                    _liffClientAccessToken = _channelAccessToken;
+                }
+                return _liffClient;
+            }
+        }
+
+        private LineMessagingClient LineClient
+        {
+            get
+            {
+                if (_lineClient == null || _lineClientAccessToken != _channelAccessToken)
+                {
+                    _lineClient = new LineMessagingClient(_channelAccessToken);
+                    _lineClientAccessToken = _channelAccessToken;
+                }
+                return _lineClient;
+            }
+        }
+
+        public ViewType SelectedViewType
+        {
+            get => _selectedViewType;
+            set
+            {
+                SetProperty(ref _selectedViewType, value);
+            }
+        }
+
+        public string SiteUrl { get => _siteUrl; set => SetProperty(ref _siteUrl, value); }
+
+        public string ChannelAccessToken { get => _channelAccessToken; set => SetProperty(ref _channelAccessToken, value); }
+
+        public string UserId { get => _userId; set => SetProperty(ref _userId, value); }
+
+        public IList<LiffApp> LiffApps { get => _liffApps; set => SetProperty(ref _liffApps, value); }
+
+        public LiffApp SelectedLiffApp { get => _selectedLiffApp; set => SetProperty(ref _selectedLiffApp, value); }
+
+        public ICommand AddLiffAppCommand { get; }
+
+        public ICommand PushLinkMessageCommand { get; }
+
+        public ICommand ListLiffAppsCommand { get; }
+
+        public ICommand DeleteLiffAppCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            AddLiffAppCommand = new DelegateCommand(async () => await AddLiffAppAsync(),
+                () => !string.IsNullOrWhiteSpace(ChannelAccessToken) && !string.IsNullOrWhiteSpace(SiteUrl))
+                .ObservesProperty(() => ChannelAccessToken)
+                .ObservesProperty(() => SiteUrl);
+
+            PushLinkMessageCommand = new DelegateCommand(async () => await PushLinkMessagAsync(),
+                () => !string.IsNullOrWhiteSpace(ChannelAccessToken) && SelectedLiffApp != null)
+                .ObservesProperty(() => ChannelAccessToken)
+                .ObservesProperty(() => SelectedLiffApp);
+
+            ListLiffAppsCommand = new DelegateCommand(async () => await ListLiffAppAsync(),
+                () => !string.IsNullOrWhiteSpace(ChannelAccessToken))
+                .ObservesProperty(() => ChannelAccessToken);
+
+            DeleteLiffAppCommand = new DelegateCommand(async () => await DeleteLiffAppAsync(),
+                () => !string.IsNullOrWhiteSpace(ChannelAccessToken) && SelectedLiffApp != null)
+                .ObservesProperty(() => ChannelAccessToken)
+                .ObservesProperty(() => SelectedLiffApp);
+
+        }
+
+        private async Task AddLiffAppAsync()
+        {
+            await LiffClient.AddLiffAppAsync(SelectedViewType, SiteUrl);
+            LiffApps = await _liffClient.GetAllLiffAppAsync();
+        }
+
+        private Task PushLinkMessagAsync() => LineClient.PushMessageAsync(_userId, $"line://app/{SelectedLiffApp.LiffId}");
+
+        private async Task ListLiffAppAsync()
+        {
+            LiffApps = await LiffClient.GetAllLiffAppAsync();
+        }
+
+        private async Task DeleteLiffAppAsync()
+        {
+            await LiffClient.DeleteLiffAppAsync(SelectedLiffApp.LiffId);
+            LiffApps = await _liffClient.GetAllLiffAppAsync();
+        }
+    }
+}
+
